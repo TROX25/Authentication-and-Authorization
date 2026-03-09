45180a511331f46fc0d963f0365adda482ef7aa9
Piotr Dziembowski <pimalogik@gmail.com>
2026-03-09 15:45:02 +0100

    Add session-based JWT token management for authentication
    Implemented session management to store JWT tokens, reducing redundant authentication requests. Updated HRManager.cshtml.cs to check and use tokens from session, and refactored authentication logic for improved efficiency. Session services and middleware are now configured in Program.cs.

--- Authentication and Authorization/Pages/HRManager.cshtml.cs
+++ Authentication and Authorization/Pages/HRManager.cshtml.cs
@@ -22,6 +22,39 @@ namespace Authentication_and_Authorization.Pages
         }
         public async Task OnGetAsync()
         {
+
+            // 1. Jesli mamy juz token to nie musze sie za kazdym razem autentykowac, moge go pobrac z Seassion
+
+            JwtToken token = new JwtToken();
+            // nazwa access_token jest dowolna, ale musi być taka sama jak ta, której używamy do przechowywania tokena w sesji.
+            var strTokenObj = HttpContext.Session.GetString("access_token");
+
+            // 2. Jesli nie mamy tokena w sesji, to musimy sie autentykowac i pobrac token z WebAPI
+            if (string.IsNullOrEmpty(strTokenObj))
+            {
+                // Authentication and getting a token from WebAPI
+                await AuthenticateAndGetTokenAsync();
+            }
+            else
+            {
+                // Jeśli token jest już dostępny w sesji, to deserializujemy go z formatu string do obiektu JwtToken, aby móc go użyć w dalszej części kodu.
+                token = JsonConvert.DeserializeObject<JwtToken>(strTokenObj) ?? new JwtToken();
+            }
+            if (token == null || token.ExpiresAt <= DateTime.Now || string.IsNullOrWhiteSpace(token.AccessToken))
+            {
+                // Jeśli token wgasł albo jest z nim jakis inny problem to znów musze go pobrać z WebAPI, czyli muszę się autentykować i pobrać nowy token.
+                await AuthenticateAndGetTokenAsync();
+            }
+
+            var client = httpClientFactory.CreateClient("TestWebAPI");
+            // Teraz mając token, chce go wysłać razem z requestem
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token?.AccessToken ?? string.Empty);
+            // WeatherForecast to nazwa kontrolera w WebAPI, który zwraca dane pogodowe. Musi być zgodna z nazwą kontrolera w WebAPI (WeatherForecastController).
+            weatherForecastItems = await client.GetFromJsonAsync<List<WeatherForecastDTO>>("WeatherForecast") ?? new List<WeatherForecastDTO>();
+        }
+
+        private async Task<JwtToken> AuthenticateAndGetTokenAsync()
+        {
             // tak samo jak client z program.cs
             var client = httpClientFactory.CreateClient("TestWebAPI");
             // DODAJE authentykację do żądania HTTP, aby uzyskać dostęp do chronionego zasobu w WebAPI. Musze zrobic post Credential
@@ -31,11 +64,7 @@ namespace Authentication_and_Authorization.Pages
             // Odczytuje zawartość odpowiedzi HTTP jako string, który powinien zawierać token JWT (JSON Web Token) zwrócony przez endpoint "Auth" w WebAPI. 
             string jwt = await response.Content.ReadAsStringAsync();
             // musze go desserializowac do formatu objektowego, aby móc go użyć w nagłówku autoryzacji.
-            var token = JsonConvert.DeserializeObject<JwtToken>(jwt);
-            // Teraz mając token, chce go wysłać razem z requestem
-            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Bearer", token?.AccessToken??string.Empty);
-            // WeatherForecast to nazwa kontrolera w WebAPI, który zwraca dane pogodowe. Musi być zgodna z nazwą kontrolera w WebAPI (WeatherForecastController).
-            weatherForecastItems = await client.GetFromJsonAsync<List<WeatherForecastDTO>>("WeatherForecast") ?? new List<WeatherForecastDTO>();
+            return JsonConvert.DeserializeObject<JwtToken>(jwt) ?? new JwtToken();
         }
     }
 }
